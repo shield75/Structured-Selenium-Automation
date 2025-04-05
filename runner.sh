27869829e7a619632b62d96499dd860beedb8004
Anisur Rahman <77926360+shield75@users.noreply.github.com>
2025-04-05 07:36:58 +0600

    Updated runner.sh file

--- runner.sh
+++ runner.sh
@@ -19,12 +19,10 @@ echo "-------------------------------------------"
 # Do not start the tests immediately. Hub has to be ready with browser nodes
 echo "Checking if hub is ready..!"
 count=0
-while [ "$( curl -s http://${HUB_HOST:-hub}:4444/status | jq -r .value.ready )" != "true" ]
-do
+while [ "$( curl -s http://${HUB_HOST:-hub}:4444/status | jq -r .value.ready )" != "true" ]; do
   count=$((count+1))
   echo "Attempt: ${count}"
-  if [ "$count" -ge 30 ]
-  then
+  if [ "$count" -ge 30 ]; then
       echo "**** HUB IS NOT READY WITHIN 30 SECONDS ****"
       exit 1
   fi
